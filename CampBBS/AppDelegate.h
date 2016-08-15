990f466b18443b6f5aa00eafaad988036d08b7c0
Zach <zach_41@163.com>
2016-08-15 14:07:21 +0800

    初始化项目

---
+++ CampBBS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CampBBS
+//
+//  Created by ZachZhang on 16/8/15.
+//  Copyright © 2016年 ZachZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+source 'https://git.oschina.net/6david9/Specs.git'
+
+target 'CampBBS' do
+    pod 'AFNetworking'
+    pod 'Masonry'
+end
+
